3195d58f0f8fb15e712f7a54db914b4ad8b3fbaa
MOHINI1403 <mohinichauhan9998@outlook.com>
2024-10-23 20:45:10 +0530

    Adding the routes for the new changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getUsersWatchHistory,
+  loginUser,
+  logOutUser,
+  registerUser,
+  updateAccountDetails,
+  updateAvatarImage,
+  updateCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { refreshAccessToken  } from "../controllers/user.controller.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 const router = Router();
 
 router.route("/register").post(
@@ -17,10 +28,25 @@ router.route("/register").post(
   registerUser
 );
 
-router.route("/login").post(
-  loginUser
-);
+router.route("/login").post(loginUser);
 // Secured Routes
-router.route("/logout").post(verifyJWT,logOutUser);
+router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refreshToken").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+//getting the information -routes
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+//update routes
+router.route("/update-accountDetails").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
+router
+  .route("/update-coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/watchHistory").get(verifyJWT,getUsersWatchHistory);
+
 export default router;
